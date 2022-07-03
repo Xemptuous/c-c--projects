5f8d991c2c67dc524c48d1bc302d5758944760f2
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2022-07-02 19:26:36 -0700

    Create enum.h

---
+++ GuiCalculator/enum.h
@@ -0,0 +1,26 @@
+enum
+{
+  ID_Hello    = 0,
+  ID_ONE      = 1,
+  ID_TWO      = 2,
+  ID_THREE    = 3,
+  ID_FOUR     = 4,
+  ID_FIVE     = 5,
+  ID_SIX      = 6,
+  ID_SEVEN    = 7,
+  ID_EIGHT    = 8,
+  ID_NINE     = 9,
+  ID_ZERO     = 10,
+  ID_DECIMAL  = 11,
+  ID_PLUS     = 12,
+  ID_MINUS    = 13,
+  ID_MULT     = 14,
+  ID_DIVIDE   = 15,
+  ID_POWER    = 16,
+  ID_ROOT     = 17,
+  ID_EQUAL    = 18,
+  ID_CLEAR    = 19,
+  ID_CLEARE   = 20,
+
+  ID_RESULT_TEXT_CONTROL = 21
+};
